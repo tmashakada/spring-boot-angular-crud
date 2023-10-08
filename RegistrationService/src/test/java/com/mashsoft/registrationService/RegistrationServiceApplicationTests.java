08bdaf2c583d13bb16411a03b18f02c0d7fc7707
Tafadzwa Mashakada <tmashakada10@gmail.com>
2023-10-08 16:52:57 +0200

    initial commit

---
+++ RegistrationService/src/test/java/com/mashsoft/registrationService/RegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashsoft.registrationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
